c01b8df5e2e0e14381292ebf61e718df828f291d
storeqn <kf324p@gmail.com>
2026-08-14 17:21:52 +0300

    Add STORE_CONFIG with store details and links

---
+++ config.js
@@ -0,0 +1,18 @@
+window.STORE_CONFIG = {
+  storeName: "الأمير براند",
+
+  whatsapp: "9647733949777",
+
+  sheetUrl:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQR4CDYOcGSOeHqY-WPr6a-dDdFGqt6f_-wxzTnIjw26haO3oeWZX4AkJ7dqIoBeXKBaNnldf1gu_8x/pub?output=csv",
+
+  currency: "د.ع",
+
+  locale: "ar-IQ",
+
+  logo: "./assets/logo.png",
+
+  instagram: "https://www.instagram.com/alameer.iq1/",
+
+  defaultImage: "./assets/logo.png"
+};
