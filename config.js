02e3f31a3951dfdf6cce950830e8c4ee2a26df5b
storeqn <kf324p@gmail.com>
2026-08-14 17:23:29 +0300

    Modify STORE_CONFIG structure in config.js
    Updated STORE_CONFIG to include cacheKey and demoProducts.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.STORE_CONFIG = {
 
   whatsapp: "9647733949777",
 
-  sheetUrl:
+  sheetCsvUrl:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vQR4CDYOcGSOeHqY-WPr6a-dDdFGqt6f_-wxzTnIjw26haO3oeWZX4AkJ7dqIoBeXKBaNnldf1gu_8x/pub?output=csv",
 
   currency: "د.ع",
@@ -14,5 +14,7 @@ window.STORE_CONFIG = {
 
   instagram: "https://www.instagram.com/alameer.iq1/",
 
-  defaultImage: "./assets/logo.png"
+  cacheKey: "alameer_products_v1",
+
+  demoProducts: []
 };
